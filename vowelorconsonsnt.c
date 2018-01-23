ff063790d8fcd40da452572ada72db17c1c4917c
Balakrishnan27 <35652976+Balakrishnan27@users.noreply.github.com>
2018-01-23 16:39:25 +0530

    Create vowelorconsonsnt.c

---
+++ vowelorconsonsnt.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char b;
+printf("enter the alphabet:");
+scanf("%c",&b);
+if(b=='a'||b=='e'||b=='i'||b=='o'||b=='u')
+printf("the alphabet is vowel");
+else
+printf("the alphabet is consonant");
+}
