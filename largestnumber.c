32cdc23ea1b5e09250c38f294a91206f480e4c9f
Balakrishnan27 <35652976+Balakrishnan27@users.noreply.github.com>
2018-01-25 16:44:50 +0530

    Update largestnumber.c

--- largestnumber.c
+++ largestnumber.c
@@ -2,12 +2,12 @@
 void main()
 {
     int a,b,c;
-    printf("enter the numbers");
+    printf("enter the numbers:");
     scanf("%d%d%d",&a,&b,&c);
     if(a>b&&a>c)
-    printf("%d is the largest number",a);
+    printf("%d \nis the largest number",a);
     else if(b>c)
-    printf("%d is the largest number",b);
+    printf("%d \nis the largest number",b);
     else
-    printf("%d is the largest number",c);
+    printf("%d \nis the largest number",c);
 }
