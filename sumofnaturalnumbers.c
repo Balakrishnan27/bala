cb95b4902483fa398eb05098c7b23e1dbd22c350
Balakrishnan27 <35652976+Balakrishnan27@users.noreply.github.com>
2018-02-01 16:38:29 +0530

    Create sumofnaturalnumbers.c

---
+++ sumofnaturalnumbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a;
+clrscr();
+printf("Enter a posiive integer n:\n");
+scanf("%d",&n);
+a=(n*(n+1))/2;
+printf("sum=%d",a);
+getch();
+}
