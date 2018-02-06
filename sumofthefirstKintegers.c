b0cd7133b496941f9e05eef1067065aded74d67d
Balakrishnan27 <35652976+Balakrishnan27@users.noreply.github.com>
2018-02-06 16:18:38 +0530

    Create sumofthefirstKintegers.c

---
+++ sumofthefirstKintegers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int b[5]={12,8,7,0,8},i,n,sum=0;
+clrscr();
+scanf("\n%d",&n);
+for(i=0;i<n;i++)
+{
+sum=sum+b[1];
+}
+printf("\n the sum is %d",sum);
+getch();
+}
