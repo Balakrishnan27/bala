96809810ea77f92d474bf4e5927761d3bc6c23ca
Balakrishnan27 <35652976+Balakrishnan27@users.noreply.github.com>
2018-02-09 16:22:25 +0530

    Create displayevennumbersbetweentwointervals.c

---
+++ displayevennumbersbetweentwointervals.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,i;
+    printf("Enter the Upper and Lower limits a and b:\n");
+    scanf("%d %d",&a,&b);
+     printf("The even numbers between %d and %d are \n",a,b);
+  for(i=a+1;i<b;i++)
+  {
+   c=i%2;
+   if(c==0)
+   printf("\n %d",i);
+    getch();
+  }
+}
