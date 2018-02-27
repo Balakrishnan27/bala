a106e67ee3263e97271f826ed712e8cf6a71a0fc
Balakrishnan27 <35652976+Balakrishnan27@users.noreply.github.com>
2018-02-27 16:32:08 +0530

    Create printthemaximumelementfromgiven.c

---
+++ printthemaximumelementfromgiven.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b[20],n,c,i;
+clrscr();
+printf("\n Enter the number:\n");
+scanf("%d",&a);
+printf("\n Enter the number:\n");
+for(i=1;i<n;i++);
+scanf("%d",&b[n]);
+c=b[0];
+for(i=1;i<n;i++);
+{
+if(b[n]>c)
+c=b[n];
+}
+printf("\n The maximum  value from given  is %d",c);
+getch();
+}
