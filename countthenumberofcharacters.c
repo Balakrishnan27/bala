a5e5d8e5cc12b084c78f42e5b964a33fe3a8eba0
Balakrishnan27 <35652976+Balakrishnan27@users.noreply.github.com>
2018-03-13 16:15:17 +0530

    Create countthenumberofcharacters.c

---
+++ countthenumberofcharacters.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[50];
+int a;
+clrscr();
+printf( "Enter string:\n ");
+printf("\n");
+gets(str);
+a= strlen(str);
+printf("\n");
+printf("\n No.of characters in string is %d " ,a );
+getch();
+}
